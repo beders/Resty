d89e08e2cec50178204802e665d1e9271e435fb0
Remi Alvergnat <remi.alvergnat@gmail.com>
2012-01-15 20:16:15 +0100

    Fixed Accept Header for xml

--- src/main/java/us/monoid/web/XMLResource.java
+++ src/main/java/us/monoid/web/XMLResource.java
@@ -146,7 +146,7 @@ public class XMLResource extends TextResource {
 	
 	@Override
 	String getAcceptedTypes() {
-		return "text/xml;application/xml;application/*+xml";
+		return "text/xml,application/xml,application/*+xml";
 	}
 
 }
