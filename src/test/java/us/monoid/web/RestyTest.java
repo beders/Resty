c8a73c6a2aae654462523d18af1edf46b8d0e708
Jochen Bedersdorfer <beders@yahoo.com>
2012-01-16 18:22:58 -0800

    Removing more live tests (geonames wasn't available)

--- src/test/java/us/monoid/web/RestyTest.java
+++ src/test/java/us/monoid/web/RestyTest.java
@@ -20,13 +20,13 @@ public class RestyTest {
 		assertEquals(r.json(getClass().getResource("test.json").toString()).json(path("key.subkey")).object().getInt("secret"), 42);
 	}
 
-	@Test
-	public void geoNames() throws Exception {
-		Resty r = new Resty();
-		Object name = r.json("http://ws.geonames.org/postalCodeLookupJSON?postalcode=66780&country=DE").get("postalcodes[0].placeName");
-		assertEquals(name, "Rehlingen-Siersburg");
-	}
-	
+//	@Test
+//	public void geoNames() throws Exception {
+//		Resty r = new Resty();
+//		Object name = r.json("http://ws.geonames.org/postalCodeLookupJSON?postalcode=66780&country=DE").get("postalcodes[0].placeName");
+//		assertEquals(name, "Rehlingen-Siersburg");
+//	}
+//	
 	@Test
 	public void formDataGet() throws Exception {
 		Resty r = new Resty();
